6c068368e6c01c7690eabec8fd291fa871cdec13
Callie Stump <callie@stu.mp>
2020-12-11 17:33:05 -0500

    adds complete functionality with user interface

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -17,23 +17,52 @@ class Program
     Console.WriteLine("Pastry's: $2");
     Console.WriteLine("Deals: 3 for $5");
     Console.WriteLine("");
-    // Bread Price Calculation
+    // Users Bread & Pastry Order
     Console.WriteLine("How many loaves of bread would you like?:");
     string breadQuantity = Console.ReadLine();
+    Console.WriteLine("How many pastry's would you like?:");
+    string pastryQuantity = Console.ReadLine();
+    //Bread Prices
     string breadPrice = "5";
     Bread newBread = new Bread(int.Parse(breadQuantity), int.Parse(breadPrice));
     int totalBreadCost = newBread.CostOfBread();
-    Console.WriteLine(totalBreadCost);
-    // Pastry Price Calculation
-    Console.WriteLine("How many pastry's would you like?:");
-    string pastryQuantity = Console.ReadLine();
+    //Pastry Prices
     string pastryPrice = "2";
     Pastry newPastry = new Pastry(int.Parse(pastryQuantity), int.Parse(pastryPrice));
     int totalPastryCost = newPastry.CostOfPastry();
-    Console.WriteLine(totalPastryCost);
-    // Total price before deals.
-    int billTotal = (totalPastryCost + totalBreadCost);
-    Console.WriteLine("Your total is:");
-    Console.WriteLine(billTotal);
+    // Total price with deals.
+    int numBreadQuan = int.Parse(breadQuantity); //Parsing line 22
+    int numPastryQuan = int.Parse(pastryQuantity); //Parsing line 24
+    if (numBreadQuan == 2 && numPastryQuan == 3) {
+      numBreadQuan = 3;
+      int newPastryCost = 5;
+      Console.WriteLine($"Cost of bread: ${totalBreadCost}.00");
+      Console.WriteLine($"Cost of pastry: ${newPastryCost}.00");
+      int finalBill = totalBreadCost + newPastryCost;
+      Console.WriteLine($"Your order total with todays deal is: ${finalBill}.00");
+    }
+    else if (numBreadQuan == 2 ) {
+      numBreadQuan = 3;
+      Console.WriteLine($"Cost of bread: ${totalBreadCost}.00");
+      Console.WriteLine($"Cost of pastry: ${totalPastryCost}.00");
+      int finalBill = totalBreadCost + totalPastryCost;
+      Console.WriteLine($"Your order total with todays deal is: ${finalBill}.00");
+    }
+    else if (numPastryQuan == 3) {
+      int newPastryCost = 5;
+      Console.WriteLine($"Cost of bread: ${totalBreadCost}.00");
+      Console.WriteLine($"Cost of pastry: ${newPastryCost}.00");
+      int finalBill = totalBreadCost + newPastryCost;
+      Console.WriteLine($"Your order total with todays deal is: ${finalBill}.00");
+    }
+    else
+    {
+      Console.WriteLine($"Cost of bread: ${totalBreadCost}.00");
+      Console.WriteLine($"Cost of pastry: ${totalPastryCost}.00");
+      int finalBill = totalBreadCost + totalPastryCost;
+      Console.WriteLine($"Your order total with todays deal is: ${finalBill}.00");
+    }
+    Console.WriteLine($"Your cart comes to {numBreadQuan} loaves of bread and {pastryQuantity} pastry's.");
+    Console.WriteLine("Thank you! Hope to see you again soon :)");
   }
 }
