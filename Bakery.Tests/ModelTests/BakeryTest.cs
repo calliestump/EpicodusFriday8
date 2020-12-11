b4f3df7688e8efa6fd010b91643421dbd0138e7a
Callie Stump <callie@stu.mp>
2020-12-11 12:06:27 -0500

    creates testing for Pastry class

--- Bakery.Tests/ModelTests/BakeryTest.cs
+++ Bakery.Tests/ModelTests/BakeryTest.cs
@@ -5,13 +5,23 @@ using Bakery;
 namespace Bakery.Tests
 {
   [TestClass]
-  public class BreadRulesTests
+  public class BreadTests
   {
     [TestMethod]
-    public void BreadRules_creates_BreadPrice()
+    public void BreadRules_creates_PastryPrice()
     {
       /*BreadRules Bread = new BreadRules("5");
       Assert.AreEqual("5", Bread.BreadPrice);*/
+    }
+  }
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryRules_creates_PastryPrice()
+    {
+      //PastryRules Pastry = new PastryRules("5");
+      
     }
   }
 }
--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -2,13 +2,20 @@ using System;
 
 namespace Bakery
 {
-  public class BreadRules
+  public class Bread
   {
     public string BreadPrice { get; set; }
-
-    public BreadRules(string breadprice)
+    public Bread(string breadprice)
     {
       BreadPrice = breadprice;
+    }
+  }
+  public class Pastry
+  {
+    public string PastryPrice { get; set; }
+    public Pastry(string pastryprice)
+    {
+      PastryPrice = pastryprice;
     }
   }
 }
