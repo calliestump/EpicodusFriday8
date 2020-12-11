c8484ae1859a326095849d14598c9dd9ea1fe2bc
Callie Stump <callie@stu.mp>
2020-12-11 14:24:07 -0500

    adds Console.WriteLine() for Program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
   static void Main()
   {
-    // program code goes here
+    Console.WriteLine("");
+    Console.WriteLine("Hello, Welcome to Pierre's Bakery!");
+    Console.WriteLine("");
+    Console.WriteLine("Menu");
+    Console.WriteLine("-------");
+    Console.WriteLine("Bread Loaf: $5");
+    Console.WriteLine("Deals: Buy 2, get 1 free!");
+    Console.WriteLine("");
+    Console.WriteLine("Pastry's: $2");
+    Console.WriteLine("Deals: 3 for $5");
+    Console.WriteLine("");
+
   }
 }
