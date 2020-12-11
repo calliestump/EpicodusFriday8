696e69154f3860f602f3b157b0a49c296871ce7f
Callie Stump <callie@stu.mp>
2020-12-10 19:59:46 -0500

    inital commit

---
+++ ToDo.Tests/ModelTests/ToDoTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project;
+
+namespace Project.Tests
+{
+  [TestClass]
+  public class ClassTests
+  {
+    [TestMethod]
+    public void Class_Action_Return()
+    {
+    
+    }
+  }
+}
